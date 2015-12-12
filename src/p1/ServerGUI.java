1d30bcb88777b15c3dc753a1ae4fb1c2b5bb40ee
MattStone23 <mattstone@cmail.carleton.ca>
2015-12-12 15:56:53 -0500

    quickfix

--- src/p1/ServerGUI.java
+++ src/p1/ServerGUI.java
@@ -45,7 +45,7 @@ public class ServerGUI {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 623, 456);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblServer = new JLabel("Server");
